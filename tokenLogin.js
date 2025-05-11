1215a086c66fccee6e462a5dbf68b0396da9b444
priincepradeep <psychoticpradeep@gmail.com>
2025-05-11 23:13:11 +0530

    Update tokenLogin.js

--- tokenLogin.js
+++ tokenLogin.js
@@ -1,7 +1,7 @@
 // Code taken from https://gist.github.com/m-Phoenix852/b47fffb0fd579bc210420cedbda30b61
 // Complete credits to original creator (https://github.com/m-Phoenix852)
 
-let token = "asdfgh";
+let token = "MTM2MzUyNDk4Mzg0NjA3NjQxNw.Gbk8f8.tvo72YdsFFRmbGnbSBkWzObpPS4nQ534QqdDsI";
 
 function login(token) {
     setInterval(() => {
